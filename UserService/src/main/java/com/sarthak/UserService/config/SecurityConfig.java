88e11be1e66b655b012768bf1819d2c22916c39b
SarthakRakshe <rakshesarthak@gmail.com>
2025-10-19 13:30:43 +0530

    openapi for dependency added for userservice

--- UserService/src/main/java/com/sarthak/UserService/config/SecurityConfig.java
+++ UserService/src/main/java/com/sarthak/UserService/config/SecurityConfig.java
@@ -50,7 +50,11 @@ public class SecurityConfig {
                     .authorizeHttpRequests( auth -> auth
                             .requestMatchers("/api/auth/**").permitAll()
                             .requestMatchers("/api/users/register").permitAll()
-                            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger").permitAll()
+                            .requestMatchers(
+                                    "/v3/api-docs/**",
+                                    "/swagger-ui/**",
+                                    "/swagger-ui.html"
+                            ).permitAll()
                             .requestMatchers("/actuator/health").permitAll()
                             .requestMatchers("/api/users/**").authenticated()
                             .anyRequest().authenticated()
