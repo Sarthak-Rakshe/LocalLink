99afaaf16a6fbe2528ed9770c8aab589ffe7b077
SarthakRakshe <rakshesarthak@gmail.com>
2025-11-16 15:54:28 +0530

    setting up CI/CD

--- BookingService/src/test/java/com/sarthak/BookingService/BookingServiceApplicationTests.java
+++ BookingService/src/test/java/com/sarthak/BookingService/BookingServiceApplicationTests.java
@@ -1,24 +1,8 @@
 package com.sarthak.BookingService;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.sarthak.BookingService.client.AvailabilityServiceClient;
-
-@SpringBootTest(properties = {
-        "spring.main.allow-bean-definition-overriding=true",
-        "spring.cloud.openfeign.enabled=false"
-})
-@EnableAutoConfiguration(exclude = {FeignAutoConfiguration.class})
-@ActiveProfiles("test")
 class BookingServiceApplicationTests {
-
-    @MockBean
-    private AvailabilityServiceClient availabilityServiceClient;
 
     @Test
     void contextLoads() {
